12e6e2dfe5779ce52e33bf8ef3609de39e839466
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2026-02-05 21:09:52 -0800

    3.2 Stack Min problem

---
+++ algorithms/StacksAndQueues/StackMin.cpp
@@ -0,0 +1,35 @@
+// 3.2 Stack Min:
+// How would you design a stack which, in addition to push and pop, has a function min
+// which returns the minimum element? Push, pop and min should all operate in O(1) time.
+#include <stack>
+#include <algorithm>
+
+class MinStack {
+    private:
+        std::stack<int> mainStack;
+        std::stack<int> minStack;
+
+        public:
+            void push(int val) {
+                mainStack.push(val);
+                
+                // the firstNumber is always inherently the min
+                if (minStack.empty() || val <= minStack.top()) {
+                    minStack.push(val);
+                }
+            }
+            void pop() { 
+                if (mainStack.empty()) return;
+
+                if (mainStack.top() == minStack.top()) {
+                    minStack.pop();
+                }
+                mainStack.pop();
+            }
+
+            int min(){
+                if (minStack.empty()) return;
+                return minStack.top();
+            }
+
+};
