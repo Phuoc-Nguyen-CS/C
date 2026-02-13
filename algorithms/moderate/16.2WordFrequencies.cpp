92655cde9037330f8c939deefc27c6130e22b9d5
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2026-02-12 22:10:24 -0800

    16.2 Word Frequency

---
+++ algorithms/moderate/16.2WordFrequencies.cpp
@@ -0,0 +1,69 @@
+// Design a method to find the frequency of occurences of any give nword in a book.
+
+#include <string>
+#include <iostream>
+#include <unordered_map>
+
+// Takes in a book and returns the number of time the word is found in the book
+// Time Complexity O(N)
+// Space Complexity O(1)
+int wordFrequency(std::string book, std::string target) {
+    
+    int found = 0;
+    std::string word = "";
+    
+    // Loop through the book and find the number of occurrences.
+    for (char c : book) {
+        if (c == ' ') {
+            if(!word.empty()) {
+                if(word == target) found++;
+                word = "";
+            }
+        }
+        else {
+            word += c;
+        }
+    }
+    return found;
+}
+
+// What if we were running this algorith multiple times?
+
+// We would then need to cache the book into a map so 
+// if they were to call it again we'd have an immediate look up.
+void wordFrequencyCache(std::string book, std::unordered_map<std::string, int>& mp) {
+    std::string word = "";
+    // Loop through the book and every word add to cache 
+    for (char c : book) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                mp[word]++;
+                word = "";
+            }
+        } else {
+            word += c;
+        }
+    }
+
+    // Gets the last word in case no space at the end.
+    if (!word.empty()) {
+        mp[word]++;
+    }
+}
+
+int main() {
+    std::unordered_map<std::string, int> mp;
+    std::string book = "  apple banana   apple orange banana apple  ";
+    int wordCount = wordFrequency(book, "apple");
+    wordFrequencyCache(book, mp);
+
+    std::cout << "--- wordFrequency O(N) ---" << std::endl;
+    std::cout << wordCount << std::endl;
+
+    std::cout << "--- Dictionary Contents ---" << std::endl;
+    for (auto const &[word, count] : mp)
+    {
+        std::cout << word << ": " << count << std::endl;
+    }
+    return 0;
+}
