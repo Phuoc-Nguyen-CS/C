1c049f4b72bd0d540acca250ec565f6305cdb2c1
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2026-02-20 15:29:54 -0800

    Car Fleet (M)

---
+++ NeetCode150/Stack/CarFleet.cpp
@@ -0,0 +1,38 @@
+#include "includes.h"
+
+class Solution
+{
+public:
+    int carFleet(int target, std::vector<int> &position, std::vector<int> &speed)
+    {
+
+        // Sort the position while retaining the speed
+        int n = position.size();
+        std::vector<std::pair<int, int>> cars(n);
+
+        for (int i = 0; i < n; ++i)
+        {
+            cars[i] = {position[i], speed[i]};
+        }
+
+        sort(cars.begin(), cars.end());
+
+        // Calculate the speed
+        std::vector<double> turnToTarget(n, 0);
+        for (int i = 0; i < n; ++i)
+        {
+            turnToTarget[i] = (double)(target - cars[i].first) / cars[i].second;
+        }
+
+        std::stack<double> st;
+        for (int i = n - 1; i >= 0; --i)
+        {
+            if (st.empty() || turnToTarget[i] > st.top())
+            {
+                st.push(turnToTarget[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
