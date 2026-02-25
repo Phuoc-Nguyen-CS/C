aa425a5dac6f6e40cb4250601c671ce990018910
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2026-02-25 14:18:43 -0800

    Reorder list (M)

---
+++ NeetCode150/LinkedList/ReorderList.cpp
@@ -0,0 +1,61 @@
+#include "includes.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    void reorderList(ListNode *head)
+    {
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        // Find the midde
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Reverse from mid to end
+        ListNode *secondHalf = slow->next;
+        slow->next = nullptr;
+        ListNode *prev = nullptr;
+        while (secondHalf != nullptr)
+        {
+            ListNode *next = secondHalf->next;
+            secondHalf->next = prev;
+
+            prev = secondHalf;
+            secondHalf = next;
+        } // Slow should now be reversed
+
+        // Combine
+        ListNode *firstHalf = head;
+        secondHalf = prev; // Reset to the top again
+        while (secondHalf != nullptr)
+        {
+            // Save
+            ListNode *tmp1 = firstHalf->next;
+            ListNode *tmp2 = secondHalf->next;
+
+            // Line in order
+            firstHalf->next = secondHalf;
+            secondHalf->next = tmp1;
+
+            // Move forward
+            firstHalf = tmp1;
+            secondHalf = tmp2;
+        }
+    }
+};
