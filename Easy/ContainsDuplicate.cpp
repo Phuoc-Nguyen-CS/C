75e5bb2ccf30855b717ed6f2ff2663115af5ad08
Phuoc Nguyen <100539667+Phuoc-Nguyen-CS@users.noreply.github.com>
2026-03-07 17:15:10 -0800

    Test for portfolio

---
+++ Easy/ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+#include "includes.h"
+
+class Solution {
+public:
+    bool hasDuplicate(std::vector<int> &nums)
+    {
+        std::unordered_set<int> seen;
+
+        for (int num : nums)
+        {
+            if (!seen.insert(num).second)
+                return true;
+        }
+
+        return false;
+    }
+};
